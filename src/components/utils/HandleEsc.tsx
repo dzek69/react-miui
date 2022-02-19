7b0b9e14dd7acc8fa9087c573d057a9f2df604f4
dzek <git-public@dzek.eu>
2022-02-19 20:08:57 +0100

    ! crashes on some components when using without `react-use` installed

--- src/components/utils/HandleEsc.tsx
+++ src/components/utils/HandleEsc.tsx
@@ -1,13 +1,14 @@
-import type React from "react";
-import { useKeyPress } from "react-use";
 import { useEffect } from "react";
+import type React from "react";
+
+import { useKeyPress } from "../../utils/useKeyPress";
 
 interface Props {
     onPress: () => void;
 }
 
 const HandleEsc: React.FC<Props> = (props) => {
-    const [pressed] = useKeyPress("Escape");
+    const pressed = useKeyPress("Escape");
 
     useEffect(() => {
         if (pressed) {
